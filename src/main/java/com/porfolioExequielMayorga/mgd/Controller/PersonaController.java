8e14171213b9158f5d6466be36aa7a05a0647828
exespartan32 <exemay777@gmail.com>
2022-08-12 18:03:02 -0300

    actualizacion

--- src/main/java/com/porfolioExequielMayorga/mgd/Controller/PersonaController.java
+++ src/main/java/com/porfolioExequielMayorga/mgd/Controller/PersonaController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author usuario
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://front-end-argentina-programa.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
